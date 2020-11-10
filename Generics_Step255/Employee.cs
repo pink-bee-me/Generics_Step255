221f4dd4ab319ede8caef776ba12deb67b08c71e
LaurieSue <pink.bee.me@outlook.com>
2020-11-10 03:25:46 -0600

    Add project files.

---
+++ Generics_Step255/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Step255
+{
+   public class Employees<T>
+
+    {
+
+       
+        public List<string> Things { get; set; }
+        public string things { get; set; }
+        
+
+        public string Add(string things)
+        {
+            return things;
+        }
+
+        public void PrintListToConsole()
+        {
+          foreach(string things in Things)
+                {
+                Console.WriteLine(things);
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Generics_Step255/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Step255
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees<string> employees = new Employees<string>();
+            employees.Things = new List<string>();
+            employees.Things.Add("barbeque");
+            employees.Things.Add("napkins");
+            employees.Things.Add("plates");
+            employees.Things.Add("wipes");
+            
+            employees.PrintListToConsole();
+
+
+
+        }
+    }
+}
